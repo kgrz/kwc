627cd219bede0a5a57adda763a3da84ce1ad60cc
Kashyap <kashyap.kmbc@gmail.com>
2017-08-15 08:33:12 +0530

    Simplify switch between straight-line reading and bufferred
    1. Check if multiple buffers are needed directly in the main function
    2. Use the straight-line process if either no filename is given, or if
       the file size is not large enough

--- main.go
+++ main.go
@@ -21,16 +21,12 @@ type chunk struct {
 	firstByte, lastByte byte
 }
 
-func (c *chunk) String() string {
+func (c chunk) String() string {
 	return fmt.Sprintf(
-		"offset: %d\nsize: %d\nwords: %d\nchars: %d\nlines: %d\nfirstByte: %d\nlastChar: %d\n",
-		c.offset,
-		c.size,
-		c.words,
+		"chars: %d\nwords: %d\nlines: %d\n",
 		c.chars,
+		c.words,
 		c.lines,
-		c.firstByte,
-		c.lastByte,
 	)
 }
 
@@ -41,74 +37,87 @@ func (c *chunk) String() string {
 const BufferSize = 4000 * 4000
 
 func main() {
+	useSingleChunk := false
+
 	if len(os.Args) < 2 {
-		stdin := bufio.NewScanner(os.Stdin)
-		chunck := process1(stdin)
-		fmt.Println("chars: ", chunck.chars)
-		fmt.Println("words: ", chunck.words)
-		fmt.Println("lines: ", chunck.lines)
+		useSingleChunk = true
+	}
+
+	if len(os.Args) == 2 {
+		filename := os.Args[1]
+		fileinfo, err := os.Stat(filename)
+		handle(err)
+		fileSize := fileinfo.Size()
+		// If the file size is smaller than the buffer size, just create one chunk.
+		// This avoids useless synchronisation cost. Even this is not optimal. This
+		// will still use mutiple CPUs for a file with size 8193 bytes. Ideally,
+		// this threshold value should be obtained by running it on a true
+		// multicore machine on files of different sizes.
+		if fileSize < BufferSize {
+			useSingleChunk = true
+		}
+	}
+
+	if useSingleChunk {
+		stream := bufio.NewScanner(os.Stdin)
+
+		if len(os.Args) == 2 {
+			f, err := os.Open(os.Args[1])
+			handle(err)
+			defer f.Close()
+			stream = bufio.NewScanner(f)
+		}
+
+		chunck := process1(stream)
+		fmt.Println(chunck)
 		os.Exit(0)
 	}
 
 	if len(os.Args) > 2 {
 		fmt.Printf("Warning: Multile files are not supported yet. Using the first one.\n\n")
+		os.Exit(1)
 	}
 
-	filename := os.Args[1]
-	f, err := os.Open(filename)
+	// FIXME: Are we reading this twice? Because we open the file to check the
+	// file size
+	f, err := os.Open(os.Args[1])
 	handle(err)
 	defer f.Close()
 
-	cpuCount := runtime.NumCPU()
-	chunks := findOffsets(f, cpuCount)
-	var finalState chunk
+	chunks := findOffsets(f)
 
-	// Fast loop if there is only one offset. This will be the case when the
-	// file size is smaller than BufferSize. Check findOffsets for more details
-	if len(chunks) > 1 {
-		var wg sync.WaitGroup
+	var wg sync.WaitGroup
 
-		fmt.Printf("Using %d cores\n\n", cpuCount)
+	for i := 0; i < len(chunks); i++ {
+		wg.Add(1)
+		go func(index int) {
+			chnkptr := &chunks[index]
+			/*
+				Note that above is not the same as:
 
-		for i := 0; i < len(chunks); i++ {
-			wg.Add(1)
-			go func(index int) {
-				chnkptr := &chunks[index]
-				/*
-					Note that above is not the same as:
+				chnk := chunks[index]
+				processBuffer(&chnk, f)
 
-					chnk := chunks[index]
-					processBuffer(&chnk, f)
+				Doing so does update that chunk object that is referenced
+				by the variable chnk, but this won't update the value in
+				the slice. So we'll need extra step of assigning the value
+				back to the slice:
 
-					Doing so does update that chunk object that is referenced
-					by the variable chnk, but this won't update the value in
-					the slice. So we'll need extra step of assigning the value
-					back to the slice:
+				chnk := chunks[index]
+				processBuffer(&chnk, f)
+				chunks[index] = chnk
 
-					chnk := chunks[index]
-					processBuffer(&chnk, f)
-					chunks[index] = chnk
-
-					Thanks, Dave Cheney!
-					https://dave.cheney.net/2017/04/29/there-is-no-pass-by-reference-in-go
-				*/
-				processBuffer(chnkptr, f)
-				wg.Done()
-			}(i)
-		}
-
-		wg.Wait()
-		finalState = reduce(chunks)
-		fmt.Println("chars: ", finalState.chars)
-		fmt.Println("words: ", finalState.words)
-		fmt.Println("lines: ", finalState.lines)
-	} else {
-		chunck := process1(bufio.NewScanner(f))
-		fmt.Println("chars: ", chunck.chars)
-		fmt.Println("words: ", chunck.words)
-		fmt.Println("lines: ", chunck.lines)
+				Thanks, Dave Cheney!
+				https://dave.cheney.net/2017/04/29/there-is-no-pass-by-reference-in-go
+			*/
+			processBuffer(chnkptr, f)
+			wg.Done()
+		}(i)
 	}
 
+	wg.Wait()
+	finalState := reduce(chunks)
+	fmt.Println(finalState)
 }
 
 // function for straightline reading. This is intended to work with
@@ -188,34 +197,27 @@ func processBuffer(chunck *chunk, f *os.File) {
 	}
 }
 
-func findOffsets(f *os.File, bufCount int) []chunk {
+func findOffsets(f *os.File) []chunk {
 	fileinfo, err := f.Stat()
 	handle(err)
 	fileSize := fileinfo.Size()
-	// If the file size is smaller than the buffer size, just create one chunk.
-	// This avoids useless synchronisation cost. Even this is not optimal. This
-	// will still use mutiple CPUs for a file with size 8193 bytes. Ideally,
-	// this threshold value should be obtained by running it on a true
-	// multicore machine on files of different sizes.
-	if fileSize < BufferSize {
-		ci := make([]chunk, 1)
-		ci[0] = chunk{size: fileSize}
-		return ci
-	}
+	cpus := runtime.NumCPU()
 
-	ci := make([]chunk, bufCount)
+	ci := make([]chunk, cpus)
 
-	size := fileSize / int64(bufCount)
-	remainder := fileSize % int64(bufCount)
+	size := fileSize / int64(cpus)
+	remainder := fileSize % int64(cpus)
 	var offset int64
 
-	for i := 0; i < bufCount; i++ {
-		if i == bufCount-1 {
+	for i := 0; i < cpus; i++ {
+		if i == cpus-1 {
 			size = size + remainder
 		}
 		ci[i] = chunk{size: size, offset: offset}
 		offset += size
 	}
+
+	fmt.Printf("Using %d cores\n\n", cpus)
 
 	return ci
 }
